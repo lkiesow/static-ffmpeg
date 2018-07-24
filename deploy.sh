b86e8597840c7a6a6c98d44a8a4b79a8ddf57e2c
darolfes <darolfes@uni-osnabrueck.de>
2018-07-24 17:08:27 +0200

    quick fix

--- deploy.sh
+++ deploy.sh
@@ -4,4 +4,8 @@ set -u
 set -e
 
 
-scp -P $DEPLOY_PORT ffmpeg_g$(git rev-parse --short HEAD)_d$(date +%s) $DEPLOY_SSH_USER@$DEPLOY_HOSTNAME:$DEPLOY_PATH
+FFMPEG_NAME=ffmpeg_g$(git rev-parse --short HEAD)_d$(date +%s)
+mv ffmpeg_bin/ffmpeg $FFMPEG_NAME
+
+echo "$DEPLOY_SSH_KEY" > ~/.ssh/id_rsa
+scp -P $DEPLOY_PORT $FFMPEG_NAME $DEPLOY_SSH_USER@$DEPLOY_HOSTNAME:$DEPLOY_PATH
