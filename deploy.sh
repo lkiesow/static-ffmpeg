fb3a7e802d78579f7bdc095b146e0a16abe45d06
darolfes <darolfes@uni-osnabrueck.de>
2018-07-24 17:32:43 +0200

    create .ssh dir

--- deploy.sh
+++ deploy.sh
@@ -7,5 +7,6 @@ set -e
 FFMPEG_NAME=ffmpeg_b$(git symbolic-ref --short HEAD)_g$(git rev-parse --short HEAD)_d$(date +%s)
 mv ffmpeg_bin/ffmpeg $FFMPEG_NAME
 
+mkdir -p ~/.ssh/id_rsa
 echo "$DEPLOY_SSH_KEY" > ~/.ssh/id_rsa
 scp -P $DEPLOY_PORT $FFMPEG_NAME $DEPLOY_SSH_USER@$DEPLOY_HOSTNAME:$DEPLOY_PATH
