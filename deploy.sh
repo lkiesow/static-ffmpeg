8efc5c142429bd15cbb8b7411b752cbf6fd94ff7
darolfes <darolfes@uni-osnabrueck.de>
2018-08-06 00:11:30 +0200

    removing branch in name + fixing dir creation

--- deploy.sh
+++ deploy.sh
@@ -4,9 +4,9 @@ set -u
 set -e
 
 
-FFMPEG_NAME=ffmpeg_b$(git symbolic-ref --short HEAD)_g$(git rev-parse --short HEAD)_d$(date +%s)
+FFMPEG_NAME=ffmpeg_g$(git rev-parse --short HEAD)_d$(date +%s)
 mv ffmpeg_bin/ffmpeg $FFMPEG_NAME
 
-mkdir -p ~/.ssh/id_rsa
+mkdir -p ~/.ssh/
 echo "$DEPLOY_SSH_KEY" > ~/.ssh/id_rsa
 scp -P $DEPLOY_PORT $FFMPEG_NAME $DEPLOY_SSH_USER@$DEPLOY_HOSTNAME:$DEPLOY_PATH
