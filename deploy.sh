dca17a484d08742f83d37f53620e189d38505e06
darolfes <darolfes@uni-osnabrueck.de>
2018-08-08 16:17:38 +0200

    add deployment script

---
+++ deploy.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+set -u
+set -e
+
+
+FFMPEG_NAME=ffmpeg_g$(git rev-parse --short HEAD)_d$(date +%s)
+mv ffmpeg_bin/ffmpeg $FFMPEG_NAME
+
+mkdir -p ~/.ssh/
+chmod 700 ~/.ssh/
+echo "$DEPLOY_SSH_KEY_BASE64" | base64 -d -i > ~/.ssh/id_rsa
+chmod 400 ~/.ssh/id_rsa
+echo "$DEPLOY_KNOWN_HOSTS" > ~/.ssh/known_hosts
+chmod 400 ~/.ssh/known_hosts
+scp -P $DEPLOY_SSH_PORT $FFMPEG_NAME $DEPLOY_SSH_USER@$DEPLOY_HOSTNAME:$DEPLOY_PATH
