05daf808b6617ef36c28ee37102bdc5f3c900456
darolfes <darolfes@uni-osnabrueck.de>
2018-08-08 14:37:13 +0200

    setting permission of .ssh files

--- deploy.sh
+++ deploy.sh
@@ -8,5 +8,9 @@ FFMPEG_NAME=ffmpeg_g$(git rev-parse --short HEAD)_d$(date +%s)
 mv ffmpeg_bin/ffmpeg $FFMPEG_NAME
 
 mkdir -p ~/.ssh/
+chmod 700 ~/.ssh/
 echo "$DEPLOY_SSH_KEY" > ~/.ssh/id_rsa
+chmod 400 ~/.ssh/id_rsa
+echo "$DEPLOY_KNOWN_HOSTS" > ~/.ssh/known_hosts
+chmod 400 ~/.ssh/known_hosts
 scp -P $DEPLOY_SSH_PORT $FFMPEG_NAME $DEPLOY_SSH_USER@$DEPLOY_HOSTNAME:$DEPLOY_PATH
