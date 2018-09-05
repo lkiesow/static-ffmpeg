29d326a79fe137f3f391f37c7ae6e3a6159941ba
darolfes <darolfes@uni-osnabrueck.de>
2018-09-05 17:21:45 +0200

    added manpages of ffmpeg/ffprobe and libav/libsw to deployment

--- deploy.sh
+++ deploy.sh
@@ -9,6 +9,12 @@ mkdir -p $FFMPEG_DIR
 
 cp ffmpeg_bin/ffmpeg $FFMPEG_DIR/.
 cp ffmpeg_bin/ffprobe $FFMPEG_DIR/.
+mkdir -p $FFMPEG_DIR/man/man1
+cp ffmpeg_build/share/man/man1/ffmpeg*   $FFMPEG_DIR/man/man1/.
+cp ffmpeg_build/share/man/man1/ffprobe*  $FFMPEG_DIR/man/man1/.
+mkdir -p $FFMPEG_DIR/man/man3
+cp ffmpeg_build/share/man/man3/libav*   $FFMPEG_DIR/man/man3/.
+cp ffmpeg_build/share/man/man3/libsw*  $FFMPEG_DIR/man/man3/.
 
 mkdir -p ~/.ssh/
 chmod 700 ~/.ssh/
