a06dfd15679411e03462b02e103fdbf64748907c
Lars Kiesow <lkiesow@uos.de>
2018-09-19 17:24:17 +0200

    Fix incorrect quoting

--- deploy.sh
+++ deploy.sh
@@ -22,4 +22,4 @@ chmod 400 ~/.ssh/id_rsa
 echo "$DEPLOY_KNOWN_HOSTS" > ~/.ssh/known_hosts
 chmod 400 ~/.ssh/known_hosts
 
-scp -P "$DEPLOY_SSH_PORT" -r "$FFMPEG_DIR $DEPLOY_SSH_USER@$DEPLOY_HOSTNAME:$DEPLOY_PATH" | true
+scp -P "$DEPLOY_SSH_PORT" -r "$FFMPEG_DIR" "$DEPLOY_SSH_USER@$DEPLOY_HOSTNAME:$DEPLOY_PATH" | true
