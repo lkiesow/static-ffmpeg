3e4ec7538f6f4323b5343a9a39fbf7d8d77c36a4
Lars Kiesow <lkiesow@uos.de>
2018-09-19 14:19:30 +0200

    Properly quote shell variables
    While it should not really be a problem in this particular case… better
    safe than sorry.

--- deploy.sh
+++ deploy.sh
@@ -4,17 +4,17 @@ set -u
 set -e
 
 
-FFMPEG_DIR=ffmpeg_g$(git rev-parse --short HEAD)_d$(date +%s)
-mkdir -p $FFMPEG_DIR
+FFMPEG_DIR="ffmpeg_g$(git rev-parse --short HEAD)_d$(date +%s)"
+mkdir -p "$FFMPEG_DIR"
 
-cp ffmpeg_bin/ffmpeg $FFMPEG_DIR/.
-cp ffmpeg_bin/ffprobe $FFMPEG_DIR/.
-mkdir -p $FFMPEG_DIR/man/man1
-cp ffmpeg_build/share/man/man1/ffmpeg*   $FFMPEG_DIR/man/man1/.
-cp ffmpeg_build/share/man/man1/ffprobe*  $FFMPEG_DIR/man/man1/.
-mkdir -p $FFMPEG_DIR/man/man3
-cp ffmpeg_build/share/man/man3/libav*   $FFMPEG_DIR/man/man3/.
-cp ffmpeg_build/share/man/man3/libsw*  $FFMPEG_DIR/man/man3/.
+cp ffmpeg_bin/ffmpeg "$FFMPEG_DIR/"
+cp ffmpeg_bin/ffprobe "$FFMPEG_DIR/"
+mkdir -p "$FFMPEG_DIR/man/man1"
+cp ffmpeg_build/share/man/man1/ffmpeg*   "$FFMPEG_DIR/man/man1"
+cp ffmpeg_build/share/man/man1/ffprobe*  "$FFMPEG_DIR/man/man1/"
+mkdir -p "$FFMPEG_DIR/man/man3"
+cp ffmpeg_build/share/man/man3/libav*    "$FFMPEG_DIR/man/man3/"
+cp ffmpeg_build/share/man/man3/libsw*    "$FFMPEG_DIR/man/man3/"
 
 mkdir -p ~/.ssh/
 chmod 700 ~/.ssh/
@@ -23,5 +23,4 @@ chmod 400 ~/.ssh/id_rsa
 echo "$DEPLOY_KNOWN_HOSTS" > ~/.ssh/known_hosts
 chmod 400 ~/.ssh/known_hosts
 
-scp -P $DEPLOY_SSH_PORT -r $FFMPEG_DIR $DEPLOY_SSH_USER@$DEPLOY_HOSTNAME:$DEPLOY_PATH | true
-
+scp -P "$DEPLOY_SSH_PORT" -r "$FFMPEG_DIR $DEPLOY_SSH_USER@$DEPLOY_HOSTNAME:$DEPLOY_PATH" | true
