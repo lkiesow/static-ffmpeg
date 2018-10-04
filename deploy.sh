4e4a12d6d761308fb281898ba60b872cd39dc7b7
Lars Kiesow <lkiesow@uos.de>
2018-10-04 18:39:23 +0200

    Deploy tarball
    Instead of deploying all files on its own, this patch creates a tarball
    containing all files.

--- deploy.sh
+++ deploy.sh
@@ -6,7 +6,7 @@ set -e
 
 DATE="$(date +%Y%m%d%H%M%S)"
 COMMIT="$(git rev-parse --short HEAD)"
-FFMPEG_DIR="ffmpeg_d${DATE}_g${COMMIT}"
+FFMPEG_DIR="ffmpeg-${DATE}-git${COMMIT}"
 mkdir -p "$FFMPEG_DIR"
 
 cp ffmpeg_bin/ffmpeg "$FFMPEG_DIR/"
@@ -15,6 +15,8 @@ mkdir -p "$FFMPEG_DIR/man/man1"
 cp ffmpeg_build/share/man/man1/ffmpeg*   "$FFMPEG_DIR/man/man1"
 cp ffmpeg_build/share/man/man1/ffprobe*  "$FFMPEG_DIR/man/man1/"
 
+tar cfJ "${FFMPEG_DIR}.tar.xz" "${FFMPEG_DIR}"
+
 mkdir -p ~/.ssh/
 chmod 700 ~/.ssh/
 echo "$DEPLOY_SSH_KEY_BASE64" | base64 -d -i > ~/.ssh/id_rsa
@@ -22,4 +24,4 @@ chmod 400 ~/.ssh/id_rsa
 echo "$DEPLOY_KNOWN_HOSTS" > ~/.ssh/known_hosts
 chmod 400 ~/.ssh/known_hosts
 
-scp -P "$DEPLOY_SSH_PORT" -r "$FFMPEG_DIR" "$DEPLOY_SSH_USER@$DEPLOY_HOSTNAME:$DEPLOY_PATH" | true
+scp -P "$DEPLOY_SSH_PORT" "${FFMPEG_DIR}.tar.xz" "$DEPLOY_SSH_USER@$DEPLOY_HOSTNAME:$DEPLOY_PATH" || true
